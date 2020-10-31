9cb7e8aa9a5cb13f448ac7664cc9e3a0e2f92337
Cecilia Bui <ceciliaylbui@gmail.com>
2020-10-31 04:29:47 +0000

    Refactor vote function

--- public/js/plants_show.js
+++ public/js/plants_show.js
@@ -6,16 +6,25 @@ const downvoteBtn = document.getElementById("downvoteBtn");
 
 
 //=======================
-// ADD EVENT LISTENERS
+// HELPER FUNCTIONS
 //=======================
-upvoteBtn.addEventListner("click", async function() {
+const sendVote = async(voteType) => {
 	// Build fetch options
 	const options = {
 		method: POST,
 		headers: {
     		'Content-Type': 'application/json'
-  		},
-		body: JSON.stringify({vote: "up"})
+  		}
+	}
+	
+	
+	if(voteType === up) {
+		options.body = JSON.stringify({vote: "up"})
+	} else if (voteType === down){
+		options.body = JSON.stringify({vote: "down"})
+	}
+	else {
+		throw "voteType must be 'up' or 'down'"
 	}
 	// Send fetch request
 	await fetch("/plants/vote", options)
@@ -28,4 +37,15 @@ upvoteBtn.addEventListner("click", async function() {
 	.catch(err =>{
 		console.log(err)
 	})
+}
+
+//=======================
+// ADD EVENT LISTENERS
+//=======================
+upvoteBtn.addEventListner("click", async function() {
+	sendVote("up")
+})
+
+downvoteBtn.addEventListner("click", async function() {
+	sendVote("down")
 })
